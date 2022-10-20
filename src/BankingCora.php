9af93770a3cd5fbb9aa79019da711785448ab0b0
divulgueregional <roseno@divulgueregional.com.br>
2022-10-20 18:01:06 -0300

    first commit

---
+++ src/BankingCora.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Divulgueregional\apicora;
+
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Message;
+use JetBrains\PhpStorm\NoReturn;
+
+class BankingCora{
+    function __construct($config)
+    {
+    }
+}
